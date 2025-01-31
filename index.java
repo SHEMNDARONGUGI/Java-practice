247715e4f43bcb0ed5f4a35d28af83ef680da9f7
SHEMNDARONGUGI <ndaroshem@gmail.com>
2025-02-01 00:15:09 +0300

    Initial commit

---
+++ index.java
@@ -0,0 +1,35 @@
+//everything should be contained inside the class
+//whetever we are writing iside these {} will be considered a part of class
+
+// JVM =>  instatiate a thread => main
+
+class index{
+    // public static void print(){
+    //     System.out.println("happy birthday");
+    // }
+    public static void main(String [] args){
+
+        int marks = 78;//4bytes
+        int roll = 34;//4bytes
+
+
+        // Data types:
+            //  integer data types
+                byte largest = Byte.MAX_VALUE;
+                byte minimum = Byte.MIN_VALUE;
+                System.out.println(largest);
+                System.out.println(minimum);
+
+                int biggest = Integer.MAX_VALUE;
+                int smallest = Integer.MIN_VALUE;
+                System.out.println(biggest);
+                System.out.println(smallest);
+
+                //#41
+
+        
+        // System.out.println("Hello world");
+        // print();
+        //Sytem.out is pointing to standard output stream
+    }
+}
