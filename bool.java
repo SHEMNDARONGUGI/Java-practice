345d6b9e7d68473280ecb34231d06bd9936df4c9
SHEMNDARONGUGI <ndaroshem@gmail.com>
2025-02-02 00:22:39 +0300

    Initial commit

---
+++ bool.java
@@ -0,0 +1,9 @@
+class bool {
+    public static void main(String[] args) {
+        boolean b = true;
+        boolean c = false;
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
--- index.java
+++ index.java
@@ -4,22 +4,46 @@
 // JVM =>  instatiate a thread => main
 
 class index{
-    // public static void print(){
-    //     System.out.println("happy birthday");
-    // }
     public static void main(String [] args){
 
         // byte(-128 - 127) > short(2 bytes) > int(4 bytes) > long (8 bytes)
 
+        byte a = Byte.MAX_VALUE;
+        byte b = Byte.MIN_VALUE;
+        System.out.println("\n For Byte:");
+        System.out.println(a);
+        System.out.println(b);
+
         short x = Short.MAX_VALUE;
         short y = Short.MIN_VALUE;
+        System.out.println("\n For Short:");
         System.out.println(x);
         System.out.println(y);
 
+        int m = Integer.MAX_VALUE;
+        int n = Integer.MIN_VALUE;
+        System.out.println("\n For Integer:");
+        System.out.println(m);
+        System.out.println(n);
+
         long i = Long.MAX_VALUE;
         long j = Long.MIN_VALUE;
+        System.out.println("\n For long:");
         System.out.println(i);
         System.out.println(j);
+
+        float f = Float.MAX_VALUE;
+        float g = Float.MIN_VALUE;
+        System.out.println("\n For float:");
+        System.out.println(f);
+        System.out.println(g);
+
+        double d = Double.MAX_VALUE;
+        double e = Double.MIN_VALUE;
+        System.out.println("\n For double:");
+        System.out.println(d);
+        System.out.println(e);
+
 
     }
 }
