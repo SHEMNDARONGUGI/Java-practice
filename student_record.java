e5d6c59d2bc352b1ce3f00e8b16bd25d4e709fa8
SHEMNDARONGUGI <ndaroshem@gmail.com>
2025-02-08 22:03:36 +0300

    Initial commit

--- student_record.java
+++ student_record.java
@@ -3,42 +3,43 @@ import java.util.Scanner;
 public class student_record {
     public static void main(String[] args) 
     {
-<<<<<<< HEAD
+
         Scanner object = new Scanner(System.in);
 
         System.out.println("Enter number of Students: ");
         int std_num = object.nextInt();
         object.nextLine();
 
+        String name;
+        String reg;
+        int age;
+
+
+
         for(int i = 1; i<= std_num;i++){
 
         System.out.println("Input name for " + i);
-=======
+
         System.out.println("Input name: ");
-        Scanner object = new Scanner(System.in);
->>>>>>> 7993413654854b739594e484d51e8aab162ef457
-        String name = object.nextLine();
+
+        name = object.nextLine();
 
         System.out.println("Input registration number: ");
-        String reg = object.nextLine();
+        reg = object.nextLine();
 
         System.out.println("Input age: ");
-        int age = object.nextInt();
-<<<<<<< HEAD
+        age = object.nextInt();
+
         object.nextLine();
 
         System.out.println();
         
-        System.out.println("Details for: " + i);
-        System.out.println("Name: "+name +"\nRegistration number: "+ reg +"\nAge: "+ age);
+        System.out.println("Details for " + name);
+        System.out.println("\nRegistration number: "+ reg +"\nAge: "+ age);
         System.out.println();
         }
         object.close();
-=======
 
-        System.out.println("Name: "+name +"\nRegistration number: "+ reg +"\nAge: "+ age);
-        
->>>>>>> 7993413654854b739594e484d51e8aab162ef457
 
     }
 }
